f25bf5e0d557c0a36a6421b8b5470ebec6e7dc7e
dembouz07 <dembouzfaye7@gmail.com>
2025-07-16 15:01:54 +0000

    update ngorklink

--- src/environments/environnement.prod.ts
+++ src/environments/environnement.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://621577d3076a.ngrok-free.app/api/v1/teachers'
+  apiUrl: 'https://fbd847acbd3e.ngrok-free.app/api/v1/teachers'
 };
